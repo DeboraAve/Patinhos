fe73b46e3730de6893fa53277bbf99c72d93f7ab
DeboraAve <debora.avelino98@gmail.com>
2023-06-11 19:52:16 -0300

    Adicionando novo print e acertando o input

--- patinhos.py
+++ patinhos.py
@@ -6,7 +6,7 @@ patinhos:int = -1 #Forçar o número a entrar na repetição de teste de código
 
 while patinhos <= 0: #função tentativa iniciada com números negativos#
     try: 
-        patinhos = int(input("Escolha o número de patinhos que foram passear: "))
+        patinhos = int(input("Escolha a quantidade de patinhos que foram passear: "))
         if patinhos <= 0:
             valorInvalido()
     except: #em caso de valor não permitido, como string, é exibida uma mensagem de erro e ele retorna a pedir a inserção do valor#
@@ -17,4 +17,5 @@ contador = patinhos
 
 while contador >= 0: #estrutura de loop criada para reproduzir a música#
     contador = validaPatinhos(contador, patinhos)
+    print("Foi um prazer cantar com você!")
 
