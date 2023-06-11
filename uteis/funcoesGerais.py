fcc9afeecbb0407e5c456abc36805b0a8053e91c
DeboraAve <debora.avelino98@gmail.com>
2023-06-11 19:47:51 -0300

    Separação de Estrofes

--- uteis/funcoesGerais.py
+++ uteis/funcoesGerais.py
@@ -1,6 +1,9 @@
 def valorInvalido():
     print("Valor inválido. Digite um número inteiro positivo!")
 
+def separaEstrofe():
+    print("=" * 15)
+
 def validaPatinhos(contador:int, patinhos:int):
     if contador > 2: #letra normal, plural#
         print(f"""{contador} patinhos 
@@ -10,6 +13,8 @@ Para brincar
 A mamãe gritou
 Quá, quá, quá, quá
 Mas só {contador-1} patinhos voltaram de lá.""")
+        separaEstrofe();
+    
     elif contador == 2: #ajuste gramatical ao final#
         print(f"""{contador} patinhos 
 foram passear
@@ -18,6 +23,8 @@ Para brincar
 A mamãe gritou
 Quá, quá, quá, quá
 Mas só {contador-1} patinho voltou de lá.""")
+        separaEstrofe();
+    
     elif contador == 1: #ajuste gramatical em toda a estrofe#
         print(f"""{contador} patinho 
 foi passear
@@ -26,6 +33,8 @@ Para brincar
 A mamãe gritou
 Quá, quá, quá, quá
 Mas nenhum patinho voltou de lá.""")
+        separaEstrofe();
+    
     else: #resultado final quando não houverem mais patinhos#
         print(f"""A mamãe patinha foi procurar
 Além das montnhas 
@@ -33,4 +42,5 @@ Na beira mar
 A mamãe gritou
 Quá, quá, quá, quá
 E os {patinhos} voltaram de lá.""")
+        separaEstrofe();
     return contador - 1
