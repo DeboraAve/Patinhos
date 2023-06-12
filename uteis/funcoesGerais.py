387a1fe727961f8bc894417e9df6ab7c6fad5ab2
DeboraAve <debora.avelino98@gmail.com>
2023-06-12 20:45:09 -0300

    Adicionando patinhos

--- uteis/funcoesGerais.py
+++ uteis/funcoesGerais.py
@@ -1,11 +1,16 @@
+import emoji
+
 def valorInvalido():
     print("Valor inválido. Digite um número inteiro positivo!")
 
-def separaEstrofe():
-    print("=" * 15)
+def separaEstrofe(contador:int):
+    print("="*8)
+    print(emoji.emojize(':duck:'*contador))
+    print("="*8)
 
 def validaPatinhos(contador:int, patinhos:int):
     if contador > 2: #letra normal, plural#
+        separaEstrofe(contador)
         print(f"""{contador} patinhos 
 foram passear
 Além das montanhas
@@ -13,9 +18,9 @@ Para brincar
 A mamãe gritou
 Quá, quá, quá, quá
 Mas só {contador-1} patinhos voltaram de lá.""")
-        separaEstrofe();
     
     elif contador == 2: #ajuste gramatical ao final#
+        separaEstrofe(contador)
         print(f"""{contador} patinhos 
 foram passear
 Além das montanhas
@@ -23,9 +28,10 @@ Para brincar
 A mamãe gritou
 Quá, quá, quá, quá
 Mas só {contador-1} patinho voltou de lá.""")
-        separaEstrofe();
+       
     
     elif contador == 1: #ajuste gramatical em toda a estrofe#
+        separaEstrofe(contador)
         print(f"""{contador} patinho 
 foi passear
 Além das montanhas
@@ -33,17 +39,17 @@ Para brincar
 A mamãe gritou
 Quá, quá, quá, quá
 Mas nenhum patinho voltou de lá.""")
-        separaEstrofe();
-    
+        
     else: #resultado final quando não houverem mais patinhos#
+        separaEstrofe(contador)
         print(f"""A mamãe patinha foi procurar
 Além das montnhas 
 Na beira mar
 A mamãe gritou
 Quá, quá, quá, quá
 E os {patinhos} voltaram de lá.""")
-        separaEstrofe();
-        print("Foi um prazer cantar com você!")
-        separaEstrofe();
-    
+        print("="*8)
+        print(emoji.emojize(':duck:'*patinhos))
+        print("="*8)
+        print("Foi um prazer cantar com você!")  
     return contador - 1
