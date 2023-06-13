e3c2007fb727d27176bbbebadb11f8c6ec8865e4
DeboraAve <debora.avelino98@gmail.com>
2023-06-13 11:50:28 -0300

    Inclusão de valor especifico de erro

--- patinhos.py
+++ patinhos.py
@@ -2,20 +2,21 @@ from uteis.funcoesGerais import valorInvalido, validaPatinhos
 
 print("Vamos cantar!")
 
-patinhos:int = -1 #Forçar o número a entrar na repetição de teste de código#
+patinhos: int = -1  # Forçar o número a entrar na repetição de teste de código#
 
-while patinhos <= 0: #função tentativa iniciada com números negativos#
-    try: 
+while patinhos <= 0:  # função tentativa iniciada com números negativos#
+    try:
         patinhos = int(input("Escolha a quantidade de patinhos que foram passear: "))
         if patinhos <= 0:
             valorInvalido()
-    except: #em caso de valor não permitido, como string, é exibida uma mensagem de erro e ele retorna a pedir a inserção do valor#
+    except ValueError:  # em caso de valor não permitido, como string, é exibida uma mensagem de erro e ele retorna a pedir a
+         # inserção do valor#
         patinhos = -1
         valorInvalido()
+    except:
+        print("Erro desconhecido.")
 
-contador = patinhos 
+contador = patinhos
 
-while contador >= 0: #estrutura de loop criada para reproduzir a música#
+while contador >= 0:  # estrutura de loop criada para reproduzir a música#
     contador = validaPatinhos(contador, patinhos)
-    
-
