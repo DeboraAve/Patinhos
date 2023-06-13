bcadc913cd4047f5dc0910deb0d480368711051e
DeboraAve <debora.avelino98@gmail.com>
2023-06-13 11:29:14 -0300

    Inserida função de atraso de print

--- uteis/funcoesGerais.py
+++ uteis/funcoesGerais.py
@@ -1,17 +1,23 @@
 import emoji
+import time
 
 def valorInvalido():
     print("Valor inválido. Digite um número inteiro positivo!")
 
-def separaEstrofe(contador:int):
+def separaEstrofe(valor:int):
+    print("\n" + "="*8)
+    print(emoji.emojize(':duck:'*valor))
     print("="*8)
-    print(emoji.emojize(':duck:'*contador))
-    print("="*8)
+
+def slowprint(texto, atraso=0.1):
+    for c in texto:
+        print(c,end='',flush=True)
+        time.sleep(atraso)
 
 def validaPatinhos(contador:int, patinhos:int):
     if contador > 2: #letra normal, plural#
         separaEstrofe(contador)
-        print(f"""{contador} patinhos 
+        slowprint(f"""{contador} patinhos 
 foram passear
 Além das montanhas
 Para brincar
@@ -21,7 +27,7 @@ Mas só {contador-1} patinhos voltaram de lá.""")
     
     elif contador == 2: #ajuste gramatical ao final#
         separaEstrofe(contador)
-        print(f"""{contador} patinhos 
+        slowprint(f"""{contador} patinhos 
 foram passear
 Além das montanhas
 Para brincar
@@ -32,7 +38,7 @@ Mas só {contador-1} patinho voltou de lá.""")
     
     elif contador == 1: #ajuste gramatical em toda a estrofe#
         separaEstrofe(contador)
-        print(f"""{contador} patinho 
+        slowprint(f"""{contador} patinho 
 foi passear
 Além das montanhas
 Para brincar
@@ -42,14 +48,12 @@ Mas nenhum patinho voltou de lá.""")
         
     else: #resultado final quando não houverem mais patinhos#
         separaEstrofe(contador)
-        print(f"""A mamãe patinha foi procurar
-Além das montnhas 
+        slowprint(f"""A mamãe patinha foi procurar
+Além das montanhas 
 Na beira mar
 A mamãe gritou
 Quá, quá, quá, quá
 E os {patinhos} voltaram de lá.""")
-        print("="*8)
-        print(emoji.emojize(':duck:'*patinhos))
-        print("="*8)
+        separaEstrofe(patinhos)
         print("Foi um prazer cantar com você!")  
     return contador - 1
